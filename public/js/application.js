027a6fdc2b0e36ce9d5f16a1bf72c5598fa061f8
Spencer Zhang <syz4@cornell.edu>
2014-07-31 13:32:00 -0500

    Implemented the frame display

--- public/js/application.js
+++ public/js/application.js
@@ -18,6 +18,11 @@ $(function() {
     $.get('/image/bounds', data, function(response) {
       console.log(response);
       
+
+      $('#sprite-movable').css('left', 0-response.x)
+      $('#sprite-movable').css('top', 0-response.y)
+      $('.sprite').css('width', response.width)
+      $('.sprite').css('height', response.height)
     })
 
   })
