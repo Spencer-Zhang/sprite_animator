743d47fe8a88174965674ea62539e4e16e055181
Spencer Zhang <syz4@cornell.edu>
2014-08-01 14:13:16 -0500

    Reorganized editor layout

--- app/models/animation.rb
+++ app/models/animation.rb
@@ -1,3 +1,7 @@
 class Animation < ActiveRecord::Base
   has_many :frames
+
+  def to_csv
+    self.frames.to_a.map(&:to_csv).join("\n")
+  end
 end
--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,4 +1,8 @@
 class Frame < ActiveRecord::Base
   belongs_to :animation
   belongs_to :spritesheet
+
+  def to_csv
+    "#{self.x}, #{self.y}, #{self.width}, #{self.height}, #{self.offset_x}, #{self.offset_y}"
+  end
 end
